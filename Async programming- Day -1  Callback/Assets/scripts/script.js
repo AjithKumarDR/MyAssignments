a8e143d521430a675f69120da08b39386d7f461b
AjithKumarDR <ajithkumardr1995@gmail.com>
2023-11-06 00:36:28 +0530

    callback hell

---
+++ Async programming- Day -1  Callback/Assets/scripts/script.js
@@ -0,0 +1,55 @@
+setTimeout(()=>{
+console.log("10")
+document.getElementById("textcon").innerText="Coundown: 10"
+document.getElementById("textb").setAttribute("style","width:80% ;display: block;")
+},1000*1)
+setTimeout(()=>{
+console.log("9")  
+document.getElementById("textcon").innerText="Coundown: 9"
+document.getElementById("textb").setAttribute("style","width:70%;display: block;")
+},1000*2)
+setTimeout(()=>{
+console.log("8")    
+document.getElementById("textcon").innerText="Coundown: 8"
+document.getElementById("textb").setAttribute("style","width:60%;display: block;")
+},1000*3)
+setTimeout(()=>{
+console.log("7")
+document.getElementById("textcon").innerText="Coundown: 7"
+document.getElementById("textb").setAttribute("style","width:50%;display: block;")
+},1000*4)
+setTimeout(()=>{
+console.log("6")
+document.getElementById("textcon").innerText="Coundown: 6"
+document.getElementById("textb").setAttribute("style","width:40%;display: block;")
+},1000*5)
+setTimeout(()=>{
+console.log("5")
+document.getElementById("textcon").innerText="Coundown: 5"
+document.getElementById("textb").setAttribute("style","width:30%;display: block;")
+},1000*6)
+setTimeout(()=>{
+console.log("4")
+document.getElementById("textcon").innerText="Coundown: 4"
+document.getElementById("textb").setAttribute("style","width:20%;display: block;")
+},1000*7)
+setTimeout(()=>{
+console.log("3")
+document.getElementById("textcon").innerText="Coundown: 3"
+document.getElementById("textb").setAttribute("style","width:10%;display: block;")
+},1000*8)
+setTimeout(()=>{
+console.log("2")
+document.getElementById("textcon").innerText="Coundown: 2"
+document.getElementById("textb").setAttribute("style","width:2px;display: block;")
+},1000*9)
+setTimeout(()=>{
+console.log("1")
+document.getElementById("textcon").innerText="Coundown: 1"
+document.getElementById("textb").setAttribute("style","width:0px; ;display: block;    border-block-color: red;")
+},1000*10)
+setTimeout(()=>{
+console.log("Happy Independance Day...!!!")
+document.getElementById("textcon").innerText="Happy Independance Day...!!!"
+document.getElementById("textb").setAttribute("style","display:none")
+},1000*11)
