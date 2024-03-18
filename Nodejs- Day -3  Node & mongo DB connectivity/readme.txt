c9e07910c53b73d4a4bd1e314e43634fbccc3c27
AjithKumarDR <ajithkumardr1995@gmail.com>
2024-03-18 05:43:47 +0530

    added postman export file

--- Nodejs- Day -3  Node & mongo DB connectivity/readme.txt
+++ Nodejs- Day -3  Node & mongo DB connectivity/readme.txt
@@ -1,2 +1,4 @@
 POSTMAN URL IS 
 "https://www.postman.com/material-technologist-39902777/workspace/my-workspace/collection/28692183-a81c5f9d-7c4a-4b46-b3bb-c987f72510d2?action=share&creator=28692183"
+
+POSTMAN WORK EXPORT FILE ADDED PLS FIND FILE NAME IS "StudentMentor.postman_collection.json"
--- Nodejs- Day -3  Node & mongo DB connectivity/src/routes/index.js
+++ Nodejs- Day -3  Node & mongo DB connectivity/src/routes/index.js
@@ -4,14 +4,9 @@ import StudentRoutes from './student.js'
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.status(200).send('<h1> Welcome to Mentor and Student Assigning with Database Site </h1>  '
-   
-    +'<H1>PostMan URL is</H1>'
-    +'<p>https://www.postman.com/material-technologist-39902777/workspace/my-workspace/collection/28692183-a81c5f9d-7c4a-4b46-b3bb-c987f72510d2?action=share&creator=28692183</p>'
-    
-    
-    
-    )
+    res.status(200).send('<h1> Welcome to Mentor and Student Assigning with Database Site </h1>  '  
+   +'<h3> POST MAN URL:"https://www.postman.com/material-technologist-39902777/workspace/my-workspace/collection/28692183-a81c5f9d-7c4a-4b46-b3bb-c987f72510d2?action=share&creator=28692183"</h3>'
+    +'<h3> POSTMAN WORK EXPORT FILE ADDED PLS FIND FILE NAME IS "StudentMentor.postman_collection.json" Please check in workfile <h3>')
 })
 router.use('/mentor',MentorRoutes)
 router.use('/student',StudentRoutes)
